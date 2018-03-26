c1a32a47a28edae16c77028eb2ec0edebed6ed3d
Daniel Wojciechowski <wojciechowski08@gmail.com>
2018-03-26 13:56:28 +0200

    adding halfMergeSort

--- Lab3/MergeSort.py
+++ Lab3/MergeSort.py
@@ -1,3 +1,5 @@
+import copy
+
 
 def executeMergeSort(data):
     """
@@ -95,41 +97,8 @@ def executeMergeSortHALF(data):
     :param list data:
     :return:
     """
-    def merge(left, right):
-        """
-        :param list left:
-        :param list right:
-        :return:
-        """
-        result = []
-        i = 0
-        j = 0
-
-        while i < len(left) and j < len(right):
-            if left[i] < right[j]:
-                result.append(left[i])
-                i += 1
-            else:
-                result.append(right[j])
-                j += 1
-        while i < len(left):
-            result.append(left[i])
-            i += 1
-        while j < len(right):
-            result.append(right[j])
-            j += 1
-        return result
-
-
-    data = data[:len(data)//2]
-
-    if len(data) < 2:
-
-        return data[:]
-
-    else:
-
-        middle = len(data)//2
-        left = executeMergeSort(data[:middle])
-        right = executeMergeSort(data[middle:])
-        return merge(left, right)
+    temp = data[len(data)//2:]
+    l = copy.copy(data)
+    l = executeMergeSort(l[:len(data)//2])
+    data = l + temp
+    return data
