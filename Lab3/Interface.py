d3b1f01f7e484be53e89ccb2e9c385c9ce582e81
Daniel Wojciechowski <wojciechowski08@gmail.com>
2018-04-02 03:29:12 +0200

    adding cos tam cos tam XD

---
+++ Lab3/Interface.py
@@ -0,0 +1,34 @@
+
+def interface():
+
+    greeting = "ALGORYTMY I STRUKTURY DANYCH\n" \
+               "LAB3 : Algorytmy sortowania\n" \
+               "Daniel Wojciechowski\n" \
+               "Wroclaw 2018\n" \
+               "\n" \
+               "Witaj!\n" \
+               "Ten program ma na celu zaprezentowanie dzialania algorytmow\n" \
+               "sortowania ciagow liczbowych w trzech roznych trybach:\n" \
+               "- Sortowanie domyslne (rosnaco)\n" \
+               "- Sortowanie odwrotne (malejaco)\n" \
+               "- Sortowanie polowy listy (rosnaco)\n"
+
+    options = "1. Wygeneruj nowy ciag liczbowy...\n" \
+              "2. Wyswietl ciag liczbowy...\n" \
+              "3. Usun ciag liczbowy...\n" \
+              "4. Dokonaj sortowania ciagu...\n" \
+              "5. \n" \
+              "0. ZAKONCZ PROGRAM"
+
+    genciag = ""
+
+    wysciag = ""
+
+    usuciag = ""
+    
+    sortujc = "Sortowanie...\n" \
+              "1. Babelkowe...\n" \
+              "2. "
+
+    print
+    u = input()
--- Lab3/tester.py
+++ Lab3/tester.py
@@ -4,7 +4,9 @@ from Lab3.BubbleSort import *
 from Lab3.SelectionSort import *
 from Lab3.MergeSort import *
 from Lab3.QuickSort import *
+from Lab3.ShellSort import *
 import copy
+import time
 
 
 test = generate(10)
@@ -15,57 +17,68 @@ print(test)
 print("\nBUBBLE SORT\n")
 
 l = copy.copy(test)
+t = time.process_time()
 executeBubbleSort(l)
+te = time.process_time() - t
 print(l)
+print(te)
 
-k = copy.copy(test)
-executeBubbleSortREV(k)
-print(k)
-
-j = copy.copy(test)
-executeBubbleSortHALF(j)
-print(j)
-
-
-# SELECTION SORT -------------------------------
-print("\nSELECTION SORT\n")
-
-l = copy.copy(test)
-executeSelectionSort(l)
-print(l)
-
-k = copy.copy(test)
-executeSelectionSortREV(k)
-print(k)
-
-j = copy.copy(test)
-executeSelectionSortHALF(j)
-print(j)
-
-
-# MERGE SORT ----------------------------------
-print("\nMERGE SORT\n")
-
-l = copy.copy(test)
-print(executeMergeSort(l))
-
-k = copy.copy(test)
-print(executeMergeSortREV(k))
-
-j = copy.copy(test)
-print(executeMergeSortHALF(j))
-
-
-# QUICK SORT ----------------------------------
-print("\nQUICK SORT\n")
-
-l = copy.copy(test)
-executeQuickSort(l)
-print(l)
-
-k = copy.copy(test)
-executeQuickSortREV(k)
-print(k)
-
-j = copy.copy(test)
-print(executeQuickSortHALF(j))
+# k = copy.copy(test)
+# executeBubbleSortREV(k)
+# print(k)
+#
+# j = copy.copy(test)
+# executeBubbleSortHALF(j)
+# print(j)
+#
+#
+# # SELECTION SORT -------------------------------
+# print("\nSELECTION SORT\n")
+#
+# l = copy.copy(test)
+# executeSelectionSort(l)
+# print(l)
+#
+# k = copy.copy(test)
+# executeSelectionSortREV(k)
+# print(k)
+#
+# j = copy.copy(test)
+# executeSelectionSortHALF(j)
+# print(j)
+#
+#
+# # MERGE SORT ----------------------------------
+# print("\nMERGE SORT\n")
+#
+# l = copy.copy(test)
+# print(executeMergeSort(l))
+#
+# k = copy.copy(test)
+# print(executeMergeSortREV(k))
+#
+# j = copy.copy(test)
+# print(executeMergeSortHALF(j))
+#
+#
+# # QUICK SORT ----------------------------------
+# print("\nQUICK SORT\n")
+#
+# l = copy.copy(test)
+# executeQuickSort(l)
+# print(l)
+#
+# k = copy.copy(test)
+# executeQuickSortREV(k)
+# print(k)
+#
+# j = copy.copy(test)
+# print(executeQuickSortHALF(j))
+#
+#
+# # SHELL SORT ---------------------------------
+# print("\nSHELL SORT\n")
+#
+# l = copy.copy(test)
+# executeShellSort(l)
+# print(l)
